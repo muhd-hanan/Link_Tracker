de4748c9413a4476c588250b291ae27c1a4f9ce6
muhd-hanan <muhdhananmkd@gmail.com>
2025-08-12 19:18:56 +0530

    23  commit

---
+++ linktraker/create_superuser.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
+
+User = get_user_model()
+
+try:
+    User.objects.create_superuser(
+        username="admin",
+        email="muhdhananmkd@gmail.com",
+        password="907255"
+    )
+    print("Superuser created")
+except IntegrityError:
+    print("Superuser already exists")
